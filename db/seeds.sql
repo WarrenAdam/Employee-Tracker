b70044ad6596a3059994a97893d4c728d283256a
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-07-20 15:42:09 -0400

    added values into department table

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ('Finance'),
+       ('Marketing'),
+       ('Sales'),
+       ('Human Resources');
+
+
+
+INSERT INTO role (title, salary)
+VALUES ('')
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('')
