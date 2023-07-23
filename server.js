4dabb070c9bb262ed682ef93ab7dc89bacca0e06
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-07-23 18:01:48 -0400

    added mysql connection

--- server.js
+++ server.js
@@ -2,4 +2,13 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
+// created MySQL connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "",
+    database: "employeeTracker_db",
+});
+
 
