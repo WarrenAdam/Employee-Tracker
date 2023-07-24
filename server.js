2ec57d0aafc35392c20b1c6eb31e1c432ba3a1cd
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-07-24 08:17:02 -0400

    added funtion to begin the app and prompts for user to decide what they would like to do.

--- server.js
+++ server.js
@@ -6,9 +6,77 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3001,
-    user: "root",
+    user: "",
     password: "",
     database: "employeeTracker_db",
 });
 
+// Begin the app
+function beginApp() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Add a Manager",
+                "Update an employee role",
+                "View Employees by Manager",
+                "View Employees by Department",
+                "Delete Departments | Roles | Employees",
+                "View the total salary of a department",
+                "Exit",
+            ],
+        })
+        .then((ans) => {
+            switch (ans.action) {
+                case "View all departments":
+                    viewDepartments();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                case "View all employees":
+                    viewEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Add a Manager":
+                    addManager();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "View Employees by Manager":
+                    viewManager();
+                    break;
+                case "View Employees by Department":
+                    viewDepartmentEmployees();
+                    break;
+                case "Delete Departments | Roles | Employees":
+                    deleteDepartmentsRolesEmployees();
+                    break;
+                case "View the total salary of a department":
+                    viewTotalSalaryDepartment();
+                    break;
+                case "Exit":
+                    connection.end();
+                    console.log("Goodbye!");
+                    break;
+            }
+        });
+}
 
